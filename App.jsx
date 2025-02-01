16b2d6d938edea6b766efbbec7939efef29ee581
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-02-01 22:06:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './component/Navbar/Navbar.jsx'
+import Hero from './component/Hero/Hero.jsx'
+import Category from "./component/Category/Category.jsx"
+
+const App = () => {
+  return (
+    <div className=''>
+      <Navbar />
+      <Hero />
+      <Category />
+    </div>
+  )
+}
+
+export default App
